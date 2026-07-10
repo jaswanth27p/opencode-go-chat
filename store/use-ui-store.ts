1381f2c680bb92ec3f18e9770286f7fcd094f799
jaswanth27p <jaswanth27p@gmail.com>
2026-07-10 17:03:35 +0530

    feat: track active thread id in ui store for sidebar highlighting

--- store/use-ui-store.ts
+++ store/use-ui-store.ts
@@ -4,10 +4,14 @@ type UiState = {
   commandMenuOpen: boolean;
   setCommandMenuOpen: (open: boolean) => void;
   toggleCommandMenu: () => void;
+  activeThreadId: string | null;
+  setActiveThreadId: (threadId: string | null) => void;
 };
 
 export const useUiStore = create<UiState>((set, get) => ({
   commandMenuOpen: false,
   setCommandMenuOpen: (open) => set({ commandMenuOpen: open }),
   toggleCommandMenu: () => set({ commandMenuOpen: !get().commandMenuOpen }),
+  activeThreadId: null,
+  setActiveThreadId: (threadId) => set({ activeThreadId: threadId }),
 }));
